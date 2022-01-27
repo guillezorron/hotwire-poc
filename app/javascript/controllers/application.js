c9b95ff6967ee9edb1599e3fdc954c1bafc6cfca
Guille Zorron <gzorron@xmartlabs.com>
2022-01-27 18:47:17 -0300

    remove unneded code

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,12 +1,10 @@
 import { Application } from "@hotwired/stimulus"
 
-import HelloController from "./hello_controller"
 import ClipboardController from "./clipboard_controller"
 import ColorsController from "./colors_controller"
 import TwitterInputController from './input_controller';
 
 const application = Application.start()
-Stimulus.register("hello", HelloController)
 Stimulus.register("clipboard", ClipboardController)
 Stimulus.register("color", ColorsControllers)
 Stimulus.register("input", TwitterInputController)
--- app/javascript/controllers/input_controller.js
+++ app/javascript/controllers/input_controller.js
@@ -4,10 +4,6 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = [ "source" ]
 
-  connect() {
-    console.log('we are liveeee')
-  }
-
   onChange(event) {
     const className = event.target.value.trim().length ? 'create-tweet-button' : 'disabled-create-tweet-button'
     this.sourceTarget.className = className
