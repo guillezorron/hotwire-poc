bae71fbd2caa7987bedf9eb49eda4b1fd0490fbc
Gonzalo Marco <gmarco2021@gmail.com>
2022-01-27 16:34:57 -0300

    stimulus change style

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -2,10 +2,12 @@ import { Application } from "@hotwired/stimulus"
 
 import HelloController from "./hello_controller"
 import ClipboardController from "./clipboard_controller"
+import ColorsController from "./colors_controller"
 
 const application = Application.start()
 Stimulus.register("hello", HelloController)
 Stimulus.register("clipboard", ClipboardController)
+Stimulus.register("color", ColorControllers)
 
 // Configure Stimulus development experience
 application.debug = false
