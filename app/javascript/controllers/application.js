49447fa982c9f2111ab51e1490fbd4370112f64a
Gonzalo Marco <gmarco2021@gmail.com>
2022-01-28 12:41:11 -0300

    stimulus - reset input field in comments

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -2,12 +2,15 @@ import { Application } from "@hotwired/stimulus"
 
 import ClipboardController from "./clipboard_controller"
 import ColorsController from "./colors_controller"
-import TwitterInputController from './input_controller';
+import TwitterInputController from './input_controller'
+import ResetFormController from "./reset_form_controller"
+
 
 const application = Application.start()
 Stimulus.register("clipboard", ClipboardController)
 Stimulus.register("color", ColorsControllers)
 Stimulus.register("input", TwitterInputController)
+Stimulus.register("reset-form", ResetFormController)
 
 // Configure Stimulus development experience
 application.debug = false
---
+++ app/javascript/controllers/reset_form_controller.js
@@ -0,0 +1,9 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="reset-form"
+export default class extends Controller {
+  reset() {
+    debugger
+    this.element.reset()
+  }
+}
